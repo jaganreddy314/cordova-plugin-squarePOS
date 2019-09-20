b1b6e30f7f09276725d08ec0d8a682b866e792c0
jagan thathagari <jaganreddy314@gmail.com>
2019-09-20 11:33:34 +1000

    added actual code

--- www/SquarePOS.js
+++ www/SquarePOS.js
@@ -5,7 +5,13 @@ var SquarePOS = "SquarePOS"; // This is just for code completion uses.
 var SquarePOS = function() {}; // This just makes it easier for
 // All of your plugin functions go below this. 
 // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
-SquarePOS.startTransction = function(onSuccess, onError) {
-   exec(onSuccess, onError, SquarePOS, "startTransction", []);
+SquarePOS.startTransaction = function(options, onSuccess, onError) {
+   var params = {
+       amount: options.amount ? options.amount : 1,
+       currencyCode: options.currencyCode? options.currencyCode : "AUD",
+       squareClientId: options.squareClientId ? options.squareClientId : "",
+       squareCallbackFunction: options.squareCallbackFunction? options.squareCallbackFunction : ""
+   };
+   return exec(onSuccess, onError, SquarePOS, "startTransaction", [params]);
 };
 module.exports = SquarePOS;
